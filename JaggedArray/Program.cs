47fc8070f20aefc2266b91509d15f5642501fc99
parikshitshelorkar <parikshitshelorkar2.0@gmail.com>
2026-09-01 13:24:30 +0530

    simple jagged array

---
+++ JaggedArray/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int[][] arr = new int[3][] {
+  new int[] {11, 21, 56},
+  new int[] {2, 5, 6, 7},
+  new int[] {2, 5}
+};
+
+Console.WriteLine("Jagged Array Elements:");
+for (int i = 0; i < arr.Length; i++)
+{
+    for (int j = 0; j < arr[i].Length; j++)
+    {
+        Console.Write(arr[i][j] + " ");
+    }
+    Console.WriteLine();
+}
